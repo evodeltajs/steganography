db83d6ec9fd2c92b3ec3e124253394425701d208
Sasha <sasha.mihalache@gmail.com>
2015-08-04 17:20:32 +0300

    Resolved one more conflicts

--- src/js/image/DownloadButton.js
+++ src/js/image/DownloadButton.js
@@ -5,21 +5,23 @@ function DownloadButton(id,canvas) {
 	var container = document.getElementById(id);	
 	var downloadBtn = document.createElement("a");
 
-	container.addEventListener("mouseenter", function() {
+ 
 	container.addEventListener("mouseover", function() {
     	
     	downloadBtn.className = "downloadBtn";
     	downloadBtn.innerHTML = "Download";
-    	
     	container.appendChild(downloadBtn);
 
-    	downloadBtn.addEventListener("click", function() {
+    });
 
-    		downloadBtn.href = canvas.toDataURL();
-    		downloadBtn.download = "image.png";
 
-    	}, false);
-	});
+    downloadBtn.addEventListener("click", function() {
+
+    	downloadBtn.href = canvas.toDataURL();
+    	downloadBtn.download = "image.png";
+
+    }, false);
+	 
 	// container.addEventListener("mouseleave",function(){
 
 	// 	container.removeChild(downloadBtn);
